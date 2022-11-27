7dd5909c84bff4b978598719c019c8e87e77f81a
Maryam Verdon <maryamverdon@gmail.com>
2022-11-27 16:58:54 +0100

    express quest 3 : POST

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -15,11 +16,15 @@ const movieHandlers = require("./movieHandlers");
 //Routes Movies
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+//created movies
+app.post("/api/movies", movieHandlers.postMovie);
 
 const usersHandlers = require("./usersHandlers");
 //Routes Users
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersId);
+//created users
+app.post("/api/users", usersHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
